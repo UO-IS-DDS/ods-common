a73ddac9c720283dc5957817f9e3d374e0b9c1ab
brockblampman <147351492+brockblampman@users.noreply.github.com>
2024-01-25 21:57:59 -0800

    .

--- models/intermediate/phones/int_banner__phones__filtered_to_active.sql
+++ models/intermediate/phones/int_banner__phones__filtered_to_active.sql
@@ -1,28 +1,16 @@
 with 
 
-banner_phones      as (select * from {{ ref('stg_banner__saturn__sprtele') }}),
+banner_phones as (select * from {{ ref('stg_banner__saturn__sprtele') }}),
 
-banner_phone_types as (select * from {{ ref('int_banner__phone_types') }}),
-
-phones_and_type_desc as (
+active_phones as (
 
   select 
 
-  -- banner_phones (driver)
-  {{ dbt_utils.star(from=ref('stg_banner__saturn__sprtele'),
-                    relation_alias='banner_phones',
-                    except=["is_active"]) }},
-
-   -- banner_phone_types
-  {{ dbt_utils.star(from=ref('int_banner__phone_types'),
-                    relation_alias='banner_phone_types',
-                    except=["ods_surrogate_key",
-                            "phone_type_code"]) }}
+    {{ dbt_utils.star(from=ref('stg_banner__saturn__sprtele'),
+                      relation_alias='banner_phones',
+                      except=["is_active"]) }}
 
   from banner_phones
-  left join banner_phone_types 
-    on banner_phone_types.phone_type_code = 
-            banner_phones.phone_type_code
   where is_active = 'Y'
 
 ),
@@ -31,11 +19,11 @@ phones_and_type_desc as (
 test_clean as (
 
   select *
-  from phones_and_type_desc t1
+  from active_phones t1
   -- failed test sql
   where t1.internal_banner_id in (
                                   select t2.internal_banner_id
-                                  from {{ ref('int_banner__entities__filtered_to_active') }} t2
+                                  from {{ ref('cln_banner__entities__filtered_to_active') }} t2
                                  )
 )
 
